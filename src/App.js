fabdb79f0073a40a458a1245f846a946bdced559
Eloim2023 <124946573+Eloim2023@users.noreply.github.com>
2023-04-19 23:28:15 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Footer from './components/footer';
+import NavBar from './components/navbar';
+import Catalog from './pages/catalog';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+//import QuantityPicker from './components/quantityPicker';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+
+      <div className='container-fluid'>
+      <Catalog/>
+      </div>
+
+      
+
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
